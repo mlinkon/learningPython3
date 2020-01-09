3db20e767652ea9f4f4915592e553533c8e56797
linkon <linkon099@gmail.com>
2020-01-09 16:23:29 +0530

    Added the program to find the second highest/maximum score, from the list in the python 3.

---
+++ BasicProblems/RunnerUpScore.py
@@ -0,0 +1,43 @@
+##Given the participants' score sheet for your University Sports Day, you are required to find the runner-up score. 
+##You are given n scores. Store them in a list and find the score of the runner-up.
+##
+##Input Format
+##
+##The first line contains n. The second line contains an array A[] of n integers each separated by a space.
+##
+##Constraints
+##2<=n<=10
+##-100<=A[i]<=100
+##Output Format
+##Print the runner-up score.
+##
+##Sample Input 0
+##5
+##2 3 6 6 5
+##
+##Sample Output 0
+##5
+##
+##Explanation 0
+##
+##Given list is
+##. The maximum score is , second maximum is . Hence, we print as the runner-up score.
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+    arr = list(arr)
+    if n<2 or n>10:
+        exit();
+    first= -100;
+    runner = -100; 
+    length = len(arr)
+    for i in range(0,length):
+        if arr[i]< -100 or arr[i]>100:
+            exit();
+        if arr[i] > first and arr[i]> runner:
+            runner = first
+            first =arr[i]
+        elif arr[i]< first and arr[i]>runner:
+            runner =arr[i]
+    print(runner) 
